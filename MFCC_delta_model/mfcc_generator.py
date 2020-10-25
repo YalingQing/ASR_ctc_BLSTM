5d91bbe842bcfdbb30c28f89b1fa32f98d012899
Yaling <qing22y@mtholyoke.edu>
2020-10-25 12:30:29 +0800

    initial commit

---
+++ MFCC_delta_model/mfcc_generator.py
@@ -0,0 +1,130 @@
+import os, string, math
+import numpy as np
+from tensorflow import keras
+from scipy.io import wavfile
+from python_speech_features import mfcc, delta
+
+class DataGenerator(keras.utils.Sequence):
+
+    def __init__(self, x, y, input_length, label_length, batch_size, nfilt):
+        self.x, self.y = x, y
+        self.input_length, self.label_length = input_length, label_length
+        self.batch_size = batch_size
+        self.nfilt = nfilt
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_x = self.x[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+        batch_input_length = self.input_length[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+        batch_label_length = self.label_length[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+
+        X = [batch_x, batch_y, batch_input_length, batch_label_length]
+        y = np.ones((len(X[0]),1))
+
+        return X,y
+
+    
+# get the audio numerical data from a given path and standardize
+def read_wav(wav_path, nfilt):
+    samplerate, audio = wavfile.read(wav_path)
+    coef = mfcc(audio, samplerate=samplerate, nfilt = nfilt, numcep = nfilt)
+    first_delta = delta(coef, 1)
+    second_delta = delta(first_delta, 3)
+    x = np.append(coef, first_delta, axis = 1)
+    x = np.append(x, second_delta, axis = 1)
+    x = (x - np.mean(x)) / np.std(x)
+    return x
+
+
+# pad all audio with -1 to a fixed length
+def pad_wav(wavs_path, nfilt):
+    audios = [read_wav(wav_path, nfilt) for wav_path in wavs_path]
+    lengths = [audio.shape[0] for audio in audios]
+    max_length = max(lengths)
+    lengths = [[length] for length in lengths]
+    audio_num = len(audios)
+    
+    X = np.ones((audio_num, max_length, nfilt*3)) * (-1)
+    for i, audio in enumerate(audios):
+        audio_len = audio.shape[0]
+        X[i,:audio_len,:] = audio
+    return X,np.array(lengths)
+
+
+# get the text from a text file path and remove all punctuation
+def read_txt(filename):
+    punctuations = [i for i in string.punctuation]
+    punctuations.append("\t")
+    punctuations.append("\n")
+    with open(filename) as reader:
+        words = reader.read().split(" ")[2:]
+        txt = " ".join(words)
+        for punctuation in punctuations:
+            txt = txt.replace(punctuation,"")
+    txt = txt.lower()
+    return np.array(list(txt))
+
+
+# construct mapping between numerical and character representation
+def dictionary():
+    chars = list(string.ascii_lowercase)
+    chars.append(" ")
+    dic_to_num, dic_to_char = {},{}
+    for i,char in enumerate(chars):
+        dic_to_num[char] = i
+        dic_to_char[i] = char
+    return dic_to_num, dic_to_char
+
+
+# convert text to numerical representation
+def text_encode(text):
+    dic,_ = dictionary()
+    encode = np.ones((text.shape[0]))
+    for i, char in enumerate(text):
+        encode[i] = dic[char]
+    return encode
+
+
+# pad all text label to fixed length
+def pad_text(txts_path):
+    texts = [text_encode(read_txt(path)) for path in txts_path]
+    lengths = [text.shape[0] for text in texts]
+    max_length = max(lengths)
+    lengths = [[length] for length in lengths]
+    text_num = len(texts)
+    
+    Y = np.ones((text_num, max_length)) * (-1)
+    for i, text in enumerate(texts):
+        text_len = text.shape[0]
+        Y[i,:text_len] = text
+    return Y,np.array(lengths)
+
+
+# get all audio file path from a given root path
+def get_wavs_path(path):
+    wavs_path = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith("wav"):
+                wav_path = os.path.join(root,name)
+                wavs_path.append(wav_path)
+    return wavs_path
+
+
+# get all corresponding text file paths
+def get_text_path(wavs_path):
+    txts_path = []
+    for wav in wavs_path:
+        (root,file) = os.path.split(wav);
+        filename = file.split(".")[0]
+        txt_path = os.path.join(root, filename+".TXT")
+        txts_path.append(txt_path)
+    return txts_path
+
---
+++ MFCC_delta_model/mfcc_model.py
@@ -0,0 +1,59 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+from keras.models import Model
+from keras.regularizers import l2
+from keras.layers import LSTM, Bidirectional, TimeDistributed, Dense, Masking, Input, Lambda, Reshape
+
+
+class brnn_ctc_mfcc():
+    def __init__(self, nfeature, nclass, lr, momentum, is_training, is_SGD):
+        self.nfeature = nfeature
+        self.nclass = nclass
+        self.lr = lr
+        self.momentum = momentum
+        self.is_training = is_training
+        self.is_SGD = is_SGD
+        self._model_init()
+        if self.is_SGD:
+            self.opt = keras.optimizers.SGD(learning_rate=self.lr, momentum = self.momentum)
+        else:
+            self.opt = keras.optimizers.Adam(learning_rate=self.lr)
+        if self.is_training:
+            self._ctc_init()
+            self.compile_ctc()
+        else:
+            self.compile_model()
+        
+    def _model_init(self):
+        self.input_data = Input(shape=(None, self.nfeature), dtype='float32',name='input')
+        mask = Masking(mask_value= -1,input_shape=(None, self.nfeature), name = "mask1")(self.input_data)
+        lstm = Bidirectional(LSTM(500, return_sequences=True, activity_regularizer = l2(1e-2), dropout = 0.4), name = "LSTM1")(mask)
+        lstm = Bidirectional(LSTM(500, return_sequences=True, activity_regularizer = l2(1e-2), dropout = 0.4), name = "LSTM2")(lstm)
+        lstm = Bidirectional(LSTM(500, return_sequences=True, activity_regularizer = l2(1e-2), dropout = 0.4), name = "LSTM3")(lstm)
+        self.output = TimeDistributed(Dense(self.nclass+1, activation='softmax'), name = "output")(lstm)
+        self.model = Model(inputs=self.input_data, outputs=self.output)
+        
+    def _ctc_init(self):
+        y_true = Input(name='y_true', shape=[None], dtype='float32')
+        input_length = Input(name='input_length_loss', shape=[1], dtype='int64')
+        label_length = Input(name='label_length', shape=[1], dtype='int64')
+        y_true_mask = Masking(mask_value= -1, name = "mask2")(y_true)
+        self.loss_out = Lambda(ctc_lambda_func, name='ctc')([y_true_mask, self.output, input_length, label_length])
+        self.ctc_model = Model(inputs=[self.input_data, y_true, input_length, label_length], outputs=[self.loss_out])
+
+    def compile_ctc(self):
+        self.ctc_model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=self.opt)
+        tf.keras.utils.plot_model(self.ctc_model, to_file="ctc_model.png")
+
+    def compile_model(self):
+        self.model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), optimizer=self.opt)
+        tf.keras.utils.plot_model(self.model, to_file="decoder_model.png")
+
+
+# model supplement
+def ctc_lambda_func(args):   
+    y_true, y_pred, input_length, label_length = args
+    return K.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+
